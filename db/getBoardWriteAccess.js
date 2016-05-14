0c8243b00eb96a46b5aa1266e3a81e6625c5422f
Edward Kim <ed@slickage.com>
2016-05-13 14:49:53 -1000

    Bug Fix: board write access scope
    Originally write access walked up the board parentage to check if any
    parent boards denined write access. This commit removes that feature.
    Now write access for boards is only limited to the board in question.

--- db/getBoardWriteAccess.js
+++ db/getBoardWriteAccess.js
@@ -8,53 +8,21 @@ var NotFoundError = Promise.OperationalError;
 module.exports = function(threadId, userPriority) {
   threadId = helper.deslugify(threadId);
 
-  var q = 'SELECT board_id FROM threads WHERE id = $1';
+  var q =
+  `SELECT b.postable_by
+  FROM boards b
+  LEFT JOIN threads t ON t.board_id = b.id
+  WHERE t.id = $1`;
   return db.sqlQuery(q, [threadId])
   .then(function(rows) {
-    if (rows.length > 0 ) { return rows[0].board_id; }
+    if (rows.length > 0 ) { return rows[0].postable_by; }
     else { throw new NotFoundError(); }
   })
-  .then(function(boardId) {
-    var q =
-    `WITH RECURSIVE find_parent(board_id, parent_id, category_id) AS (
-      SELECT bm.board_id, bm.parent_id, bm.category_id
-      FROM board_mapping bm where board_id = $1
-      UNION
-      SELECT bm.board_id, bm.parent_id, bm.category_id
-      FROM board_mapping bm, find_parent fp
-      WHERE bm.board_id = fp.parent_id
-    )
-    SELECT
-      fp.board_id,
-      fp.parent_id,
-      fp.category_id,
-      b.postable_by as board_postable,
-      c.postable_by as cat_postable
-    FROM find_parent fp
-    LEFT JOIN boards b on fp.board_id = b.id
-    LEFT JOIN categories c on fp.category_id = c.id`;
-    return db.sqlQuery(q, [boardId])
-    .then(function(rows) {
-      var postable = false;
-      if (rows.length < 1) { return postable; }
-
-      var boardPostable = true;
-      var catsPostable = true;
-      rows.forEach(function(row) {
-        var boardPriority = row.board_postable;
-        if (typeof boardPriority === 'number' && userPriority > boardPriority) {
-          boardPostable = false;
-        }
-
-        var catPriority = row.cat_postable;
-        if (typeof catPriority === 'number' && userPriority > catPriority) {
-          catsPostable = false;
-        }
-      });
-
-      if (boardPostable && catsPostable) { postable = true; }
-      return postable;
-    });
+  .then(function(postable_by) {
+    var postable = false;
+    if (postable_by === null || postable_by === undefined) { postable = true; }
+    else if (typeof postable_by === 'number' && userPriority <= postable_by) { postable = true; }
+    return postable;
   })
   .error(function() { return false; });
 };
